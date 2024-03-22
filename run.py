07f3a770f9dbbdaab5be6aec1e6da580f08c6421
matgus217 <matgus217@gmail.com>
2024-03-22 11:50:41 +0000

    Add user name and print in run.py

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
 import random
+
+def get_user_name():
+
+    while True:
+        print('Welcome to guess number game')
+        print('You can only guess numbers, best of luck!\n')
+        print("Enter your name:")
+        name = input()
+        print(f"Hi {name}! Let's play the game!\n")
+        break
