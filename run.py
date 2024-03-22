70ccd43c156d505e006dead85f6339d9b2b52d1a
matgus217 <matgus217@gmail.com>
2024-03-22 11:52:32 +0000

    Add function that generates random number in run.py

--- run.py
+++ run.py
@@ -9,3 +9,24 @@ def get_user_name():
         name = input()
         print(f"Hi {name}! Let's play the game!\n")
         break
+
+        def user_guess(x):
+    """
+    Function that generates a random number between 1 and 20
+    """
+    while True:
+        try:
+            number = random.randint(1, x)
+            user_guess = 0
+            while user_guess != number:
+                user_guess = int(input(f'Make a guess between 1 and {x}:\n'))
+
+                if (user_guess < number):
+                    print('Sorry you guessed too low, try again!\n')
+                elif (user_guess > number):
+                    print('Sorry you guessed too high, try again!\n')
+                else:
+                    print(f'You guessed correct, it was {number}!\n')
+            break
+        except ValueError:
+            print('Only enter numbers please, try again:')
